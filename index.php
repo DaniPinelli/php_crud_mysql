accad9dee615e31e123dcd2f77c14853bf3358d5
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-10-18 16:19:34 -0300

    Creating the form

--- index.php
+++ index.php
@@ -6,8 +6,30 @@ include("includes/header.php");
 include("includes/footer.php");
 ?>
 
+    <div class="container p-4">
 
-    <h1>Hello World</h1>
+        <div class="col-md-4" >
+
+        <div class="card card-body" >
+            <form action="" >
+                <div class="form-group" >
+                    <input type="text" name="title" class="form-control"
+                    placeholder="Title" autofocus>
+
+                </div>
+
+            </form>
+
+        </div>
+
+
+        </div>
+
+        <div class="col-md-8" >
+
+        </div>
+
+    </div>
 
 
 
